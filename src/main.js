3da0f511f580364e8314590e162c5e2d944431db
Romain Monteil <monteil.romain@gmail.com>
2025-07-30 16:30:55 +0200

    Reproducer for issue 1719

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+import { Application } from "@hotwired/stimulus"
+
+import DialogController from "./controllers/dialog-controller.js"
+
+window.Stimulus = Application.start()
+Stimulus.register("dialog", DialogController)
