2886d864fa2daa51f65ea4b51bb32c30bbc2c6d0
Romain Monteil <monteil.romain@gmail.com>
2025-07-30 16:26:33 +0200

    Init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
 import './style.css'
-
-import { Application } from "@hotwired/stimulus"
-
-import DialogController from "./controllers/dialog-controller.js"
-
-window.Stimulus = Application.start()
-Stimulus.register("dialog", DialogController)
